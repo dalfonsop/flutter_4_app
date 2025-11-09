b99e6bdb6a3e2f3974e344c5e78b88c2dea231af
dalfonsop <dalfonsop94@gmail.com>
2025-11-08 23:40:37 -0500

    Add App tutorial

--- widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,4 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+    title: 'Busca la comida',
+    caption: 'dfhgdfhfgh hfgh fghdfgh  fghfgh fghdfgh',
+    imageUrl: 'assets/images/1.png',
+  ),
+  SlideInfo(
+    title: 'Entrega rápida',
+    caption:
+        'fghfgh fghfgh fghfgh fghfgh fghfh fhfh fghfhfhfh fghfhfh fgh ghhh fhfgh',
+    imageUrl: 'assets/images/2.png',
+  ),
+  SlideInfo(
+    title: 'Disfruta la comida',
+    caption: 'fhgfgh fgh  fghfghfgh fghfgh fghfgh',
+    imageUrl: 'assets/images/3.png',
+  ),
+];
 
 class AppTutorialScreen extends StatelessWidget {
   const AppTutorialScreen({super.key});
@@ -7,6 +39,64 @@ class AppTutorialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            physics: BouncingScrollPhysics(),
+            children: slides
+                .map(
+                  (data) => _Slide(
+                    title: data.title,
+                    caption: data.caption,
+                    imageUrl: data.imageUrl,
+                  ),
+                )
+                .toList(),
+          ),
+          Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(child: Text('Salir'),onPressed: () => context.pop(),)
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+
+    return Padding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Image(image: AssetImage(imageUrl)),
+          SizedBox(height: 20,),
+          Text(title, style: titleStyle,),
+          SizedBox(height: 10,),
+          Text(caption, style: captionStyle,)
+          
+          ]),
+      ),
+    );
   }
 }
